73744d17ea55445b8452a28970ec56ca63c304dc
priscilla ma <priscillama621@gmail.com>
2022-01-23 18:35:17 -0500

    added characters object containing all char types. added empty strings. added char length prompt. removed ! and return from var lowercase, uppercase, etc. and made it so the code is only concerned with adding to allChars when user confirms they want that char type.

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,52 @@
-// Assignment Code
+// Assignment Code               // nothing is happening up here bc these r all var and functions. //
 var generateButton = document.querySelector("#generate");
+var characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "1234567890",
+  special: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+};
 
-// Write password to the #password input
+// Starts to write password when called aka clicked.
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password"); 
-
-  passwordText.value = password;
-
+  var password = generatePassword(); //function
+  var passwordText = document.querySelector("#password"); //textarea = passwordText. this is just selecting the textarea.
+  passwordText.value = password; //.value replaces the textarea box with the eventual generated password.
 }
 
 function generatePassword(){
-  // var length = prompt("how long would you like your password to be? (please choose between 8-128 characters)");
-  var lowercase = confirm("would you like to include lowercase characters?");
-    if(!lowercase){
-      return;
-    }
-  var uppercase = confirm("would you like to include uppercase letters?");
-    if(!uppercase){
-      return;
-    }
-  var numeric = confirm("would you like to include numeric characters?");
-    if(!numeric){
-      return;
-    }
-  var specialChar = confirm("would you like your password to include special characters?");
-    if(!specialChar){
-      return;
-    }
+  var allChars = ""; //empty string to store all character types. isn't randomized yet
+  var finalPassword = ""; //contains randomized index turned into chars. chars r continuously added in a loop for however long the user inputs the length of (8-128) characters for. each loop is a character combined for the final pw.
+  var length = prompt("how long would you like your password to be? (please choose between 8-128 characters)");
+    if(length < 8 || length > 128){ //user entered invalid number.. alert will show.
+      alert("please choose a number between 8 and 128");
+      generatePassword(); //calls function again to ask length prompt again.
+    } else { //otherwiseeeeee. keeps going with pop up boxes if user entered valid number.
+        var lowercase = confirm("would you like to include lowercase characters?");
+          if(lowercase){ //if confirm (press OK), continues w pop up box questions, and adds lowercase character type to empty string.
+            allChars += characters.lowercase; // starts adding char type to empty string.
+          }
+        var uppercase = confirm("would you like to include uppercase letters?");
+          if(uppercase){
+            allChars += characters.uppercase; // another char type.
+          }
+        var numeric = confirm("would you like to include numeric characters?");
+          if(numeric){
+            allChars += characters.numeric;
+          }
+        var specialChar = confirm("would you like your password to include special characters?");
+          if(specialChar){
+            allChars += characters.special;
+          }
+        if(!lowercase && !uppercase && !numeric && !specialChar){ //if user did not confirm any char types
+          alert("please choose at least one character type");
+        }
+      }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 // Add event listener to generate button
 generateButton.addEventListener("click", writePassword); //calling the already assigned function writePassword
 // when you click the button it's going to start creating a password -> this is when the prompt should show up.
-
-
 
 
 

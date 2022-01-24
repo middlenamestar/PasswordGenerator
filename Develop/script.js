3e6da27f85d8466acb90222f5123b19d02c578a5
priscilla ma <priscillama621@gmail.com>
2022-01-23 20:09:12 -0500

    edited comments and took out notes to self

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
-// Assignment Code               // nothing is happening up here bc these r all var and functions. //
+// Selects the button id and assigns it a variable
 var generateButton = document.querySelector("#generate");
+// Object containing all character types
 var characters = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -7,28 +8,37 @@ var characters = {
   special: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
 };
 
-// Starts to write password when called aka clicked.
+// Starts to write password when called a.k.a clicked
 function writePassword() {
-  var password = generatePassword(); //function
-  var passwordText = document.querySelector("#password"); //textarea = passwordText. this is just selecting the textarea.
-  passwordText.value = password; //.value replaces the textarea box with the eventual generated password.
+  // Function
+  var password = generatePassword();
+  // Selects textarea (from html) and assigns a variable
+  var passwordText = document.querySelector("#password");
+  // .value replaces the textarea box with the eventual generated password
+  passwordText.value = password;
 }
 
+// This is where all the good stuff is happenin'!
 function generatePassword(){
-  var allChars = ""; //empty string to store all character types. isn't randomized yet
-  var finalPassword = ""; //contains randomized index turned into chars. chars r continuously added in a loop for however long the user inputs the length of (8-128) characters for. each loop is a character combined for the final pw.
+  // Empty string to store all character types. Is not randomized yet.
+  var allChars = "";
+  // Contains randomized characters
+  var finalPassword = "";
+  // Starts prompting user
   var length = prompt("how long would you like your password to be? (please choose between 8-128 characters)");
-    if(length < 8 || length > 128){ //user entered invalid number.. alert will show.
+    if(length < 8 || length > 128){
       alert("please choose a number between 8 and 128");
-      generatePassword(); //calls function again to ask length prompt again.
-    } else { //otherwiseeeeee. keeps going with pop up boxes if user entered valid number.
+      // Calls this function again
+      generatePassword();
+    } else {
         var lowercase = confirm("would you like to include lowercase characters?");
-          if(lowercase){ //if confirm (press OK), continues w pop up box questions, and adds lowercase character type to empty string.
-            allChars += characters.lowercase; // starts adding char type to empty string.
+          if(lowercase){
+            // Adds character type to the empty string
+            allChars += characters.lowercase;
           }
         var uppercase = confirm("would you like to include uppercase letters?");
           if(uppercase){
-            allChars += characters.uppercase; // another char type.
+            allChars += characters.uppercase;
           }
         var numeric = confirm("would you like to include numeric characters?");
           if(numeric){
@@ -38,61 +48,19 @@ function generatePassword(){
           if(specialChar){
             allChars += characters.special;
           }
-        if(!lowercase && !uppercase && !numeric && !specialChar){ //if user did not confirm any char types
+        if(!lowercase && !uppercase && !numeric && !specialChar){
           alert("please choose at least one character type");
         }
       }
-  for (var i = 0; i < length; i++) { //for loop you can use on string or array //this is for the length
-  //execute the codes in here as many times as the length in the condition
-    var indexNumber = Math.floor(Math.random() * allChars.length); //returns the index number. you have a long ass string now added with a lot of diff char types, this formula is only going to choose 1 number from the now long ass string, but it's ok bc that's what the loop is for - the loops will keep choosing another number to add to the final empty string.
-    var character = allChars.charAt(indexNumber); //returns the actual character of the specified index, so the finalPassword will have actual letters and whatnot, not just obscure index numbers.
-    finalPassword += character; //we're adding the characters returned to the finalPassword empty string.
+  // Use for loop for the length of the allChars string and apply Math.floor Math.random to get indexes
+  for (var i = 0; i < length; i++) {
+    var indexNumber = Math.floor(Math.random() * allChars.length);
+    // Returns the actual character of the specified index
+    var character = allChars.charAt(indexNumber);
+    // Adding characters to the finalPassword string
+    finalPassword += character;
   }
   return finalPassword;
 }
 
-// Add event listener to generate button
-generateButton.addEventListener("click", writePassword); //calling the already assigned function writePassword
-// when you click the button it's going to start creating a password -> this is when the prompt should show up.
-
-
-
-////////////
-// series of prompts for password criteria:
-//how long do you want your password to be (between 8 and 128 char)?
-//do you want to include lowercase letters?
-//do you want to include uppercase letters?
-//do you want your password to include numbers?
-// do you want to include special characters? (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
-
-//when user hits cancel to reject lowercase, uppercase, etc..
-//it's still going to go to the next question...
-//but in my code when user hits OK - it needs to track that data e.g.
-//user said ok to lowercase... if(prompt)
-
-
-// each character type would be an array so if they want lowercase
-//that just means the mathRandom is going to include one of the arrays (character types)
-//to generate randomly. like the array would be ["a", "b", "c"...]
-//uppercase array would be "[A], [B], [C]..."
-
-//the final password .. I think I want to have it display in the text .. so .. .textContent
-//I think... use getElementById or querySelector to target the place you want to put the text
-//and add .textContent onto it and the final password goes there.
-
-// FROM MDN: let text = document.getElementById('divA').textContent;
-// FROM MDN: The text variable is now: 'This is some text!'
-
-//thinking about how... if user hits cancel, how to NOT log the lowercase, uppercase information?
-//probably just else? and then like move on to the next question lol...
-//but if user hits OK, it would be if statement and then some sort of function I think with the
-//mathRandom shit........... 
-
-
-
-//ALSO BEFORE U GET STARTED, U NEED TO CLONE A NEW REPO DOWN FROM GITHUB.
-//;)
-
-
-// id #password TEXTAREA
-// id #generate BUTTON
+generateButton.addEventListener("click", writePassword);
